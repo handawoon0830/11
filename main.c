165dfd128d0c16d31b6a2319887956ca969b3def
handawoon0830 <handawoon0830@naver.com>
2023-11-16 16:08:18 +0900

    5

--- main.c
+++ main.c
@@ -4,41 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int i;
-	int grade[5];
-	int average;
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for (i=0; i<5; i++)
-	{
-		printf("grade[%i]= ", i);
-		scanf("%d", &grade[i]);
-	}
-	/*
-	int sum = 0;
+	pc= (char *)10000;
+	pi= (int*)10000;
+	pd = (double *)10000; // 자료형의 바이트에 따라 증가. 
 	
-	for (i=0; i<5; i++)
-	{
-		int *j = &grade[i];
-		printf("grade[%i]= %i\n", i, *j);
-		sum += *j;
-	}
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
-	int average;
-	
-	average= sum/5;
-	printf(" average= %i\n",average);
-	*/
-	
-	for (i=0; i<5; i++)
-	{
-		printf("grade[%i]= %i\n", i, *(grade+i));
-		average= (average + *(grade +i));
-		
-	}
-	
-	average= average/5;
-	
-	printf(" average= %i\n",average);
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	return 0;
 }
 
